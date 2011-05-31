b2102ee55dcb63216f884f1dfd324df699b77717
Thomas Murach <asiasuppenesser@gmx.de>
2011-05-31 13:17:43 +0200

    delta suchen

--- Ue5/main.cpp
+++ Ue5/main.cpp
@@ -18,51 +18,73 @@ typedef double **matrix;
 void geom_pbc();
 inline double p(double phi_re, double phi_im, double B_re, double B_im, double phi2, double lambda){ return exp(2*(B_re*phi_re+B_im*phi_im) - phi2 - lambda*(phi2-1)*(phi2-1)); }
 
-#define n_mc_runs 10000
+#define n_mc_runs 1e5
 
 int main(){
   TRandom3 *ran = new TRandom3(0);
-  double phi_re, phi_im, B_re, B_im, p_phi, lambda, phi2, phi2_neu, kappa, r1, r2;
+  double phi_re, phi_im, B_re, B_im, p_phi, phi2, phi2_neu, kappa, r1, r2;
   phi_re = ran->Uniform();
   phi_im = ran->Uniform();
   kappa = ran->Uniform();
-  lambda = 0;//ran->Uniform();
+  double lambda[2]={0, ran->Uniform()};
 
   B_re = ran->Uniform();        // fuer uns soll B erstmal konstant sein
   B_im = ran->Uniform();
+  double B2 = B_re*B_re + B_im*B_im;
 
   double phi_neu_re, phi_neu_im, p_phi_neu;
   double p_accept;
   double p_grenz = 0.5;
-  double delta = 0.5;
-  double phi_mean_re = 0., phi_mean_im = 0.;
+  double delta = 2.;
+  double phi_mean_re=0., phi_mean_im=0., phi2_mean=0.;
+  double dummy_re=0., dummy_im=0.;
+  int counter;
 
-  for (int k=0; k<n_mc_runs; ++k){
-    for (int i=0; i<10; ++i){
-      r1 = ran->Uniform(-1*delta, delta);
-      r2 = ran->Uniform(-1*delta, delta);
+  for (int j=0; j<2; ++j){
+    counter = 0;
+    for (int k=0; k<n_mc_runs; ++k){
+      for (int i=0; i<10; ++i){
+        r1 = ran->Uniform(-1*delta, delta);
+        r2 = ran->Uniform(-1*delta, delta);
 
-      phi2 = phi_re*phi_re + phi_im*phi_im;
-      p_phi = p(phi_re, phi_im, B_re, B_im, phi2, lambda);
+        phi2 = phi_re*phi_re + phi_im*phi_im;
+        p_phi = p(phi_re, phi_im, B_re, B_im, phi2, lambda[j]);
 
-      phi_neu_re = phi_re + r1;
-      phi_neu_im = phi_im + r2;
+        phi_neu_re = phi_re + r1;
+        phi_neu_im = phi_im + r2;
 
-      phi2_neu = phi_neu_re*phi_neu_re + phi_neu_im*phi_neu_im;
-      p_phi_neu = p(phi_neu_re, phi_neu_im, B_re, B_im, phi2_neu, lambda);
+        phi2_neu = phi_neu_re*phi_neu_re + phi_neu_im*phi_neu_im;
+        p_phi_neu = p(phi_neu_re, phi_neu_im, B_re, B_im, phi2_neu, lambda[j]);
 
-      p_accept = (p_phi_neu >= p_phi) ? 1 : p_phi_neu/p_phi;
+        p_accept = (p_phi_neu >= p_phi) ? 1 : p_phi_neu/p_phi;
 
-      if(p_accept==1 || p_phi_neu/p_phi>p_grenz ) {phi_re = phi_neu_re; phi_im = phi_neu_im;}
+        if(p_accept==1 || p_phi_neu/p_phi>p_grenz ) {phi_re = phi_neu_re; phi_im = phi_neu_im; phi2 = phi2_neu; counter++;}
+      }
+      phi_mean_re += phi_re;
+      phi_mean_im += phi_im;
+      phi2_mean += phi2;
+      dummy_re += phi_re*(phi2-1);
+      dummy_im += phi_im*(phi2-1);
     }
-    phi_mean_re += phi_re;
-    phi_mean_im += phi_im;
-  }
-  phi_mean_re /= n_mc_runs;
-  phi_mean_im /= n_mc_runs;
+    phi_mean_re /= n_mc_runs;
+    phi_mean_im /= n_mc_runs;
+    phi2_mean /= n_mc_runs;
+    dummy_re /= n_mc_runs;
+    dummy_im /= n_mc_runs;
 
-  cout << phi_mean_re << "  " << B_re << endl;
-  cout << phi_mean_im << "  " << B_im << endl;
+    cout << endl << "Akzeptanz: " << counter*1./(10*n_mc_runs) << endl;
+
+    if (j == 0){
+      cout << endl << "######### Checks for lambda = 0: #########" << endl;
+      cout << "phi_re: " << phi_mean_re << "  , B_re: " << B_re << endl;
+      cout << "phi_im: " << phi_mean_im << "  , B_im: " << B_im << endl;
+      cout << "phi2  : " << phi2_mean << "   , 1+|B|2: " << 1 + B2 << endl << endl;
+    } else {
+      cout << endl << "######### Checks for lambda > 0: #########" << endl;
+      cout << "phi_re: " << phi_mean_re << ", Re(B-2*lambda*(...)): " << B_re - 2*lambda[j]*dummy_re << endl;
+      cout << "phi_im: " << phi_mean_im << ", Im(B-2*lambda*(...)): " << B_im - 2*lambda[j]*dummy_im << endl << endl;
+    }
+  }
 }
 
 
