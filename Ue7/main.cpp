1fa81a43dd0d0e841f237ae446ed1d727eba02bb
Thomas Murach <murach@physik.hu-berlin.de>
2011-06-14 00:52:39 +0200

    geom anfang

--- Ue7/main.cpp
+++ Ue7/main.cpp
@@ -43,9 +43,17 @@ void  get5(FILE *file);
 void  getf5(FILE *file);
 
 #define n_mc_runs 1000
+#define N 100
+#define ndim 2
+
+int **nn;
+int lsize[4] = {0,N,N,N};
+int nvol;
 
 int main(){
+  geom_pbc();
   TRandom3 *ran = new TRandom3(0);
+  
   double phi_re, phi_im, B_re, B_im, p_phi, phi2, phi2_neu, kappa, r1, r2;
   phi_re = ran->Uniform();
   phi_im = ran->Uniform();
@@ -845,4 +853,64 @@ void getf5(FILE *file){
       fscanf(file, "%le\n", &blksum[ivar][ib]);
   for(ivar=0; ivar<nvar; ivar++)
     fscanf(file, "%le\n", &sqsum[ivar]);
+}
+
+void geom_pbc(){
+  /*
+  Angelegt und besetzt ist                            B Bunk 12/2005
+  Dimension     ndim                              rev     4/2011
+  Gittergroesse lsize[k], k=1..ndim
+
+  Angelegt und berechnet wird
+  Volumen       nvol
+  NN-Indexfeld  nn[k][i], k=0..2*ndim, i=0..(nvol-1)
+
+  nn[k][i] gibt den Index des Nachbarn von i in Richtung +k,
+  unter Beruecksichtigung periodischer Randbedingungen.
+  Fuer einen Schritt in Richtung -k setze man den Index auf (ndim+k).
+  nn[i][0] ist reserviert.
+  */
+  int   i, k;
+  int *ibase, *ix; 
+
+  ibase = (int *) malloc((ndim+2) * sizeof(int));
+  ix = (int *) malloc((ndim+1) * sizeof(int));
+
+  /* Basis fuer Punktindizes */
+  ibase[1] = 1;
+  for (k=1; k<=ndim; k++) ibase[k+1] = ibase[k]*lsize[k];
+  nvol = ibase[ndim+1];
+
+  if (nn) free(nn[0]);
+  free(nn);
+  nn = (int **) malloc((2*ndim+1) * sizeof(int *));
+  nn[0] = (int *) malloc(nvol*(2*ndim+1) * sizeof(int));
+  for (k=1; k<=2*ndim; k++) nn[k] = nn[0] + nvol*k;
+
+  for (k=1; k<=ndim; k++) ix[k] = 0;   /* Koord. des Anfangspunkts */
+
+  for (i=0; i<nvol; i++){           /* Schleife ueber Punkte */
+    nn[0][i] = 0;
+    for (k=1; k<=ndim; k++){
+      nn[k][i] = i + ibase[k];      /* Nachbar x + e_k */
+      if (ix[k] == (lsize[k]-1)){
+	nn[k][i] -= ibase[k+1];
+	nn[0][i] = 1;		    /* für Dirichlet-RB */
+      }
+
+      nn[ndim+k][i] = i - ibase[k]; /* Nachbar x - e_k */
+      if (ix[k] == 0){
+	nn[ndim+k][i] += ibase[k+1];
+	nn[0][i] = 1;		    /* für Dirichlet-RB */
+      }
+//       cout << "geomfkt: " << nn[k][i] << " " << nn[ndim+k][i] << endl;
+    }
+
+    for (k=1; k<=ndim; k++){        /* Koord. des naechsten Punkts */
+      ix[k]++;
+      if (ix[k] < lsize[k]) break;
+      ix[k] = 0;
+    }
+  }
+  free(ibase); free(ix);
 }
