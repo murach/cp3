5cdd0981c035d8bc014847c2b3a650d7f56f15ae
Thomas Murach <asiasuppenesser@gmx.de>
2011-06-14 13:56:43 +0200

    6 besser mit ran->Uniform()

--- Ue6/main.cpp
+++ Ue6/main.cpp
@@ -89,7 +89,7 @@ int main(){
 
         p_accept = (p_phi_neu >= p_phi) ? 1 : p_phi_neu/p_phi;
 
-        if(p_accept==1 || p_phi_neu/p_phi>p_grenz ) {phi_re = phi_neu_re; phi_im = phi_neu_im; phi2 = phi2_neu; counter++;}
+        if(p_accept==1 || p_phi_neu/p_phi>ran->Uniform() ) {phi_re = phi_neu_re; phi_im = phi_neu_im; phi2 = phi2_neu; counter++;}
       }
       accum5(1, phi_re);
       accum5(2, phi_im);
